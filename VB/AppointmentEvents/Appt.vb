64ce63eeb90ed2230f0a25ba558e4facf25a3e44
DevExpressExampleBot <fakeEmail>
2019-04-19 16:11:50 +0300

    Source auto update [skip ci]

---
+++ VB/AppointmentEvents/Appt.vb
@@ -0,0 +1,75 @@
+﻿Imports DevExpress.Mvvm
+Imports DevExpress.UI.Xaml.Scheduler
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace AppointmentEvents
+	Public Class Appt
+		Inherits BindableBase
+
+		Protected _subject As String
+		Public Property Subject() As String
+			Get
+				Return Me._subject
+			End Get
+			Set(ByVal value As String)
+				Me.SetProperty(Me._subject, value, "Subject")
+			End Set
+		End Property
+
+
+		Protected _start As DateTime
+		Public Property Start() As DateTime
+			Get
+				Return Me._start
+			End Get
+			Set(ByVal value As DateTime)
+				Me.SetProperty(Me._start, value, "Start")
+			End Set
+		End Property
+
+
+		Protected _end As DateTime
+		Public Property [End]() As DateTime
+			Get
+				Return Me._end
+			End Get
+			Set(ByVal value As DateTime)
+				Me.SetProperty(Me._end, value, "End")
+			End Set
+		End Property
+
+		Protected _patternId As Object
+		Public Property PatternId() As Object
+			Get
+				Return Me._patternId
+			End Get
+			Set(ByVal value As Object)
+				Me.SetProperty(Me._patternId, value)
+			End Set
+		End Property
+
+		Protected _recurrenceRule As String
+		Public Property RecurrenceRule() As String
+			Get
+				Return Me._recurrenceRule
+			End Get
+			Set(ByVal value As String)
+				Me.SetProperty(Me._recurrenceRule, value, "RecurrenceRule")
+			End Set
+		End Property
+
+		Protected _type As AppointmentType
+		Public Property Type() As AppointmentType
+			Get
+				Return Me._type
+			End Get
+			Set(ByVal value As AppointmentType)
+				Me.SetProperty(Me._type, value)
+			End Set
+		End Property
+	End Class
+End Namespace
---
+++ VB/AppointmentEvents/MainPage.xaml.vb
@@ -0,0 +1,126 @@
+﻿Imports DevExpress.Core.Native.Events
+Imports DevExpress.UI.Xaml.Scheduler
+Imports System
+Imports System.Collections.ObjectModel
+Imports Windows.UI.Popups
+Imports Windows.UI.Xaml.Controls
+
+Namespace AppointmentEvents
+	Public NotInheritable Partial Class MainPage
+		Inherits Page
+
+		Public Sub New()
+			Me.InitializeComponent()
+			Me.DataContext = Me
+		End Sub
+
+
+		Protected _items As ObservableCollection(Of Appt)
+		Public ReadOnly Property Items() As ObservableCollection(Of Appt)
+			Get
+				If Me._items Is Nothing Then
+					Me._items = New ObservableCollection(Of Appt)()
+					Me._items.Add(New Appt() With {
+						.Start = DateTime.Today,
+						.End = DateTime.Today.AddHours(2),
+						.Subject = "Appointment1"
+					})
+					Me._items.Add(New Appt() With {
+						.Start = DateTime.Today.AddHours(4),
+						.End = DateTime.Today.AddHours(6),
+						.Subject = "Appointment2"
+					})
+					Me._items.Add(New Appt() With {
+						.Start = DateTime.Today.AddHours(7),
+						.End = DateTime.Today.AddHours(9),
+						.Subject = "Recurrent Appt",
+						.PatternId = "Pattern1",
+						.Type = AppointmentType.Pattern,
+						.RecurrenceRule = RecurrenceRule.[New]().Daily()
+					})
+				End If
+
+				Return Me._items
+			End Get
+		End Property
+
+
+		Protected _logItems As ObservableCollection(Of String)
+		Public ReadOnly Property LogItems() As ObservableCollection(Of String)
+			Get
+				If Me._logItems Is Nothing Then
+					Me._logItems = New ObservableCollection(Of String)()
+				End If
+
+				Return Me._logItems
+			End Get
+		End Property
+
+		Private Sub SchedulerControl_AppointmentAdding(ByVal sender As Object, ByVal e As DevExpress.UI.Xaml.Scheduler.AppointmentAddingEventArgs)
+			Log("AppointmentAdding")
+			If chkAdding.IsChecked.Value Then
+				Confirm(e, "Do you want to proceed adding a new appointment?")
+			End If
+		End Sub
+
+		Private Sub SchedulerControl_AppointmentAdded(ByVal sender As Object, ByVal e As DevExpress.UI.Xaml.Scheduler.AppointmentAddedEventArgs)
+			Log("AppointmentAdded")
+		End Sub
+
+		Private Sub SchedulerControl_AppointmentUpdating(ByVal sender As Object, ByVal e As DevExpress.UI.Xaml.Scheduler.AppointmentUpdatingEventArgs)
+			Log("AppointmentUpdating")
+			If chkUpdating.IsChecked.Value Then
+				Confirm(e, "Do you want to save changes?")
+			End If
+		End Sub
+
+		Private Sub SchedulerControl_AppointmentUpdated(ByVal sender As Object, ByVal e As DevExpress.UI.Xaml.Scheduler.AppointmentUpdatedEventArgs)
+			Log("AppointmentUpdated")
+		End Sub
+
+		Private Sub SchedulerControl_AppointmentRemoving(ByVal sender As Object, ByVal e As DevExpress.UI.Xaml.Scheduler.AppointmentRemovingEventArgs)
+			Log("AppointmentRemoving")
+			If chkRemoving.IsChecked.Value Then
+				Confirm(e, "Do you want to delete this appointment?")
+			End If
+		End Sub
+
+		Private Sub SchedulerControl_AppointmentRemoved(ByVal sender As Object, ByVal e As DevExpress.UI.Xaml.Scheduler.AppointmentRemovedEventArgs)
+			Log("AppointmentRemoved")
+		End Sub
+
+		Private Sub SchedulerControl_RemoveAppointmentDialogShowing(ByVal sender As Object, ByVal e As DevExpress.UI.Xaml.Scheduler.RemoveAppointmentDialogShowingEventArgs)
+			Log("RemoveAppointmentDialogShowing")
+			If chkEditDialogShowing.IsChecked.Value Then
+				Confirm(e, "Do you want to show RemoveAppointmentDialog?")
+			End If
+		End Sub
+
+		Private Sub SchedulerControl_EditAppointmentDialogShowing(ByVal sender As Object, ByVal e As DevExpress.UI.Xaml.Scheduler.EditAppointmentDialogShowingEventArgs)
+			Log("EditAppointmentDialogShowing")
+			If chkRemoveDialogShowing.IsChecked.Value Then
+				Confirm(e, "Do you want to open EditAppointmentDialog?")
+			End If
+		End Sub
+
+		Private Async Sub Confirm(ByVal args As DeferredCancelEventArgs, ByVal message As String)
+			Dim deferral = args.GetDeferral()
+			Dim dialog = New MessageDialog(message, "Confirmation")
+			dialog.Commands.Add(New UICommand() With {
+				.Label = "Yes",
+				.Id = 0
+			})
+			dialog.Commands.Add(New UICommand() With {
+				.Label = "No",
+				.Id = 1
+			})
+			Dim result = Await dialog.ShowAsync()
+			args.Cancel = CInt(Math.Truncate(result.Id)) = 1
+			deferral.Complete()
+		End Sub
+
+		Private Sub Log(ByVal message As String)
+			Me.LogItems.Insert(0, DateTime.Now.ToString("HH:mm:ss") & " >>> " & message)
+		End Sub
+	End Class
+End Namespace
